97507b2c51d41c86c783f372360dcfb99fd6a489
NEPTLIANG <NEPTLIANG@outlook.com>
2018-02-06 22:23:23 +0800

    写了个用于练习咏春防守的程序

---
+++ XuYiMiao/XuYiMiao.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+main()
+{
+	for(;;)
+	{
+printf("                                                       111111             SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+printf("                                                      1111111         SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+printf("                                                     11111111      SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+printf("                                                    111111111     SSSS\n");
+printf("                                                    111111111    SSSSS\n");
+printf("                                                       111111    SSSSS\n");
+printf("                                                       111111     SSSSS\n");
+printf("                                                       111111      SSSSS\n");
+printf("    -----------------------------------------          111111        SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+printf("    -----------------------------------------          111111          SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+printf("    -----------------------------------------          111111            SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+printf("                                                       111111                                                   SSSSS\n");
+printf("                                                       111111                                                    SSSSS\n");
+printf("                                                       111111                                                     SSSSS\n");
+printf("                                                       111111                                                     SSSSS\n");
+printf("                                                       111111                                                    SSSSS\n");
+printf("                                                       111111                                                   SSSSS\n");
+printf("                                                       111111                                                 SSSSS\n");
+printf("                                                       111111     SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+printf("                                                    111111111111    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n");
+printf("                                                    111111111111       SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\n\n\n\n\n\n");
+	}
+}
