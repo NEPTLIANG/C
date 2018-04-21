bbf57c9a8bbcddf055ff32dc893473266c9fdbf0
NEPTLIANG <NEPTLIANG@outlook.com>
2018-04-21 18:07:22 +0800

    Daily Backup

---
+++ StructPrac1/cmake-build-debug/CMakeFiles/StructPrac1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StructPrac1.dir/StruPrac1.cpp.o"
+  "StructPrac1.pdb"
+  "StructPrac1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StructPrac1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
