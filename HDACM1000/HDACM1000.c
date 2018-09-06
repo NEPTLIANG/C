6abd0e40e52e7a2c422809a079c72253155e0e0f
NEPTLIANG <NEPTLIANG@outlook.com>
2018-09-06 11:56:26 +0800

    尝试做杭电OJ的第1000号题目

--- HDACM1000/HDACM1000.c
+++ HDACM1000/HDACM1000.c
@@ -1,4 +1,37 @@
 #include <stdio.h>
-main()
+int main()
 {
-
+    int i , j , k , l ;
+    char a[20] , b[20] , c[30] ;
+    for ( i=0 ; i<20&&a[i]!=' ' ; i++ )
+    {
+        scanf("%c" , &a[i]) ;
+        if ( a[i]==' ' ) { break ; }
+        //printf("%d",i);
+    }
+    //printf("a OK");
+    for ( j=0 ; j<20&&b[j]!=10 ; j++ )
+    {
+        scanf("%c" , &b[j]) ;
+        if ( b[j]==10 ) { break ; }
+        //printf("%d",j);
+    }
+    //printf("b OK");
+    //for(int x=0;x<j;x++){printf("%c",b[x]);}
+    for ( k=1 ; k<=i&&k<=j ; k++ )
+    {
+        if ( a[i-k]+b[j-k]-'0'> '9')
+        {
+            c[30-k] = a[i-k] + b[j-k] - ('9'+1) ;
+            c[30-k-1] ++ ;
+        }
+        else
+        {
+            c[30-k] = a[i-k] + b[j-k] - '0' ;
+        }
+    }
+    for ( l=29-k ; l>29-i||l>29-j ; l-- )
+    {
+        printf("%c" , c[l]) ;
+    }
+}
